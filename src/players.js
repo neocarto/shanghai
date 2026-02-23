90e2b20539f59431089c4dc194066e97f5433777
neocarto <nicolas.lambert@cnrs.fr>
2026-02-23 12:05:05 +0100

    fix img louis

--- src/players.js
+++ src/players.js
@@ -14,7 +14,7 @@ export const predefinedPlayersRiate = [
   {
     id: 5,
     name: "Louis CLXXX",
-    avatar: import.meta.env.BASE_URL + "img/louis.jpeg",
+    avatar: import.meta.env.BASE_URL + "img/louis.jpg",
   },
   {
     id: 7,
